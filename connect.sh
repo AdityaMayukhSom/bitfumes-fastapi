46905b8f63c967eaf2e737644f47ba5279506a1e
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-29 12:41:14 +0530

    FIX: command to connect to mysql command line

---
+++ connect.sh
@@ -0,0 +1,4 @@
+# run this command in terminal with mariadb-client-core installed
+# to connect to the running mysql database from command line
+
+mysql -h 127.0.0.1 -P 6969 --protocol=tcp -u devstream -p
